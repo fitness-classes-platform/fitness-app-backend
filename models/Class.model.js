7a49e98b0d38eabc452fbb66c337336ed379a761
Mariaesteves <mariaferraesteves@hotmail.com>
2025-05-22 13:53:26 +0100

    fix review route and add a validate to contacts in the model Class

--- models/Class.model.js
+++ models/Class.model.js
@@ -28,6 +28,12 @@ const classSchema = new Schema({
         },
         contacts: {
                 type: String,
+                validate: {
+                        validator: function (v) {
+                            return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v); // Example for email validation
+                        },
+                        message: (props) => `${props.value} is not a valid contact!`,
+                    },
         },
 
         reviews: [{
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const Class= require("../models/Class.model")
-const Review= require("../models/Review.model");
+const Class = require("../models/Class.model")
+const Review = require("../models/Review.model");
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 
 
@@ -10,95 +10,103 @@ const { isAuthenticated } = require("../middleware/jwt.middleware");
 
 router.get("/class", (req, res, next) => {
   Class.find()
-  .populate("reviews")
-  .then((classes)=>{
-    res.status(200).json(classes)
-  })
-  .catch((error)=>{
-    res.status(500).json({message: "Server Error"})
-    next(error);
-  })
+    .populate("reviews")
+    .then((classes) => {
+      res.status(200).json(classes)
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Server Error" })
+      next(error);
+    })
 });
 
 router.post("/class", (req, res, next) => {
-  const newClass= req.body;
+  const newClass = req.body;
 
   Class.create(newClass)
-  .then((newClass)=>{
-    res.status(201).json(newClass)
-  })
-  .catch((error)=>{
-    res.status(500).json({message: "Error creating new Class"})
-    next(error);
-  })
+    .then((newClass) => {
+      res.status(201).json(newClass)
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Error creating new Class" })
+      next(error);
+    })
 });
 
 router.delete("/class/:classId", isAuthenticated, (req, res, next) => {
-  const {classId} = req.params;
+  const { classId } = req.params;
 
   Class.findByIdAndDelete(classId)
-  .then((deleteClass)=>{
-    res.status(204).json(deleteClass)
-  })
-  .catch((error) => {
-    res.status(500).json({ error: "Error deleting class" })
-    next(error);
-  })
+    .then((deleteClass) => {
+      res.status(204).json(deleteClass)
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Error deleting class" })
+      next(error);
+    })
 })
 
 router.get("/class/:classId", (req, res, next) => {
-  const {classId} = req.params;
+  const { classId } = req.params;
 
   Class.findById(classId)
-  .populate("reviews")
-  .then((findClass)=>{
-    res.status(200).json(findClass)
-  })
-  .catch((error) => {
-    res.status(500).json({ error: "Error displaying class" })
-    next(error);
-  })
+    .populate("reviews")
+    .then((findClass) => {
+      res.status(200).json(findClass)
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Error displaying class" })
+      next(error);
+    })
 })
 
 router.put("/class/:classId", isAuthenticated, (req, res, next) => {
-  const {classId} = req.params;
-  const newDetails= req.body;
+  const { classId } = req.params;
+  const newDetails = req.body;
 
-  Class.findByIdAndUpdate(classId, newDetails, {new:true})
-  .then((updateClass)=>{
-    res.status(200).json(updateClass)
-  })
-  .catch((error) => {
-    res.status(500).json({ error: "Error updating class" })
-    next(error);
-  })
+  Class.findByIdAndUpdate(classId, newDetails, { new: true })
+    .then((updateClass) => {
+      res.status(200).json(updateClass)
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Error updating class" })
+      next(error);
+    })
 
 })
 
 // Review Routes
 router.get("/review/class/:classId", (req, res, next) => {
   Review.find()
-  .then((reviews)=>{
-    res.status(200).json(reviews)
-  })
-  .catch((error)=>{
-    res.status(500).json({message: "Server Error"})
-    next(error);
-  })
+    .then((reviews) => {
+      res.status(200).json(reviews)
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Server Error" })
+      next(error);
+    })
 });
 
 router.post("/review/class/:classId", isAuthenticated, (req, res, next) => {
-  const newReview= req.body;
-  const {classId}= req.params;
+  const { classId } = req.params;
+  const { title, description, ranking } = req.body;
 
-  Review.findByIdAndUpdate(classId)
-  .then((newReview)=>{
-    res.status(201).json(newReview)
-  })
-  .catch((error)=>{
-    res.status(500).json({message: "Error creating new review"})
-    next(error);
-  })
+ 
+  Review.create({ title, description, ranking })
+    .then((newReview) => {
+      return Class.findByIdAndUpdate(
+        classId,
+        { $push: { reviews: newReview._id } },
+        { new: true }
+      ).populate("reviews");
+    })
+    .then((updatedClass) => {
+      res.status(201).json(updatedClass);
+    })
+    .catch((error) => {
+      console.error("Error adding review:", error);
+      res.status(500).json({ error: "Failed to add review" });
+    });
 });
 
 /*router.delete("/review/:reviewId", isAuthenticated, (req, res, next) => {
@@ -115,16 +123,16 @@ Review.findByIdAndDelete(reviewId)
 })*/
 
 router.get("/review/:reviewId", (req, res, next) => {
-  const {reviewId} = req.params;
+  const { reviewId } = req.params;
 
   Review.findById(reviewId)
-  .then((findReview)=>{
-    res.status(200).json(findReview)
-  })
-  .catch((error) => {
-    res.status(500).json({ error: "Error displaying review" })
-    next(error);
-  })
+    .then((findReview) => {
+      res.status(200).json(findReview)
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Error displaying review" })
+      next(error);
+    })
 })
 
 /*router.put("/review/:reviewId", isAuthenticated, (req, res, next) => {
